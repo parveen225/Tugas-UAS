dc8e0902bcc491d0804318b053ec93cabe9405eb
lufikaZkl30 <149558662+lufikaZkl30@users.noreply.github.com>
2023-12-06 21:12:52 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-// button on
+
+// navbar stay
+$(document).ready(function() {
+  var navbar = $(".navbar");
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= 100) {
+      navbar.addClass("fixed-top");
+    } else {
+      navbar.removeClass("fixed-top");
+    }
+  });
+});
+
+
+
+
+// button on to halaman baru
   document.getElementById('btn-explore').addEventListener('click', function() {
     window.location.href = 'index.html';
 });
