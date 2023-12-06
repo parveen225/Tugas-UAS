1a4f5b677e8792f7c990f6dce54f42ea560d4a32
lufikaZkl30 <149558662+lufikaZkl30@users.noreply.github.com>
2023-12-06 12:13:38 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-document.getElementById('next').onclick = function(){
-  let lists = document.querySelectorAll('.item');
-  document.getElementById('slide').appendChild(lists[0]);
-}
-document.getElementById('prev').onclick = function(){
-  let lists = document.querySelectorAll('.item');
-  document.getElementById('slide').prepend(lists[lists.length - 1]);
-}
-
 // button on
   document.getElementById('btn-explore').addEventListener('click', function() {
     window.location.href = 'index.html';
